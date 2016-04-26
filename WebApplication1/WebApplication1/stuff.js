e5b87569c002dcf98c2a7c20e2cdf4e712348ddf
Ted Doss <tededoss@gmail.com>
2016-04-26 09:20:17 -0600

    fixed trash code, added css style sheet, and a button that changes the background to a random color.

--- WebApplication1/WebApplication1/stuff.js
+++ WebApplication1/WebApplication1/stuff.js
@@ -1,3 +1,9 @@
 ﻿onload = function () {
     document.getElementsByTagName("P")[0].innerHTML = "Hello World!";
 }
+
+function randomBackGround() {
+    var rColor = Math.round(Math.random() * 16 * 16 * 16 * 16 * 16 * 16);
+    document.getElementsByTagName('p')[0].innerText = "#" + rColor.toString(16);
+    document.getElementsByTagName('body')[0].style.backgroundColor = "#"+rColor.toString(16);
+}
